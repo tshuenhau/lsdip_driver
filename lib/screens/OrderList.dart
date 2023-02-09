f86e9b40eb09cbd76018b45db821a99f4d4dc1e2
tshuenhau <tshuenhau@gmail.com>
2023-02-09 23:00:50 +0800

    Setup general widgets structure

---
+++ lib/screens/OrderList.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class OrderList extends StatefulWidget {
+  const OrderList({super.key});
+
+  @override
+  State<OrderList> createState() => _OrderListState();
+}
+
+class _OrderListState extends State<OrderList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
