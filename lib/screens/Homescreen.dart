345af134464b7915b61877d17875a5024ea3513f
tshuenhau <tshuenhau@gmail.com>
2023-03-09 21:23:57 +0800

    test gps miles calculation

--- lib/screens/Homescreen.dart
+++ lib/screens/Homescreen.dart
@@ -5,6 +5,7 @@ import 'package:lsdip_driver/screens/OrdersScreen.dart';
 import 'package:lsdip_driver/screens/VehicleScreen.dart';
 import 'package:lsdip_driver/widgets/layout/CustomBottomNavigationBar.dart';
 import 'package:lsdip_driver/widgets/layout/CustomPageView.dart';
+import 'dart:math' show cos, sqrt, asin;
 
 class Homescreen extends StatefulWidget {
   Homescreen({required this.vehicleId, super.key});
@@ -21,6 +22,16 @@ class _HomescreenState extends State<Homescreen> {
   late double lat = 0;
   late double long = 0;
   late int count = 0;
+  double totalDistance = 0;
+
+  double calculateDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
+  }
 
   List<GeoPoint> points = [];
 
@@ -67,6 +78,16 @@ class _HomescreenState extends State<Homescreen> {
 
   @override
   Widget build(BuildContext context) {
+    for (var i = 0; i < points.length - 1; i++) {
+      setState(() {
+        totalDistance += calculateDistance(
+            points[i].latitude,
+            points[i].longitude,
+            points[i + 1].latitude,
+            points[i + 1].longitude);
+      });
+    }
+    print(totalDistance);
     List<Widget> _navScreens = [
       OrdersScreen(lat: lat, long: long),
       VehicleScreen(vehicleId: widget.vehicleId)
